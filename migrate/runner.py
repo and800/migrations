a4a1d43b745d54268be085738f261d3b234738b9
Andriy Maletsky <andriy.maletsky@gmail.com>
2016-09-03 16:07:40 +0300

    state get/set functions

--- migrate/runner.py
+++ migrate/runner.py
@@ -1,7 +1,11 @@
 import time
 import os
+import json
+import fnmatch
 
 migrations_dir = 'migrations/'
+state_file = migrations_dir + '.state.json'
+state = None
 
 
 def create(name):
@@ -18,3 +22,32 @@ def down():
     filename = '{}_{}'.format(now, name.replace(' ', '_'))
     with open('{}{}.py'.format(migrations_dir, filename), 'w') as file:
         file.write(template)
+
+
+# def perform(direction='up', name=None):
+#     get_state()
+
+
+def get_state():
+    global state
+    available = [
+        file
+        for file in os.listdir(migrations_dir)
+        if fnmatch.fnmatch(file, '*.py')
+    ]
+    available.sort(key=lambda name: int(name.split('_', maxsplit=1)[0]))
+    try:
+        with open(state_file, 'r') as file:
+            performed = json.load(file)
+    except FileNotFoundError:
+        performed = []
+    state = [(file, file in performed) for file in available]
+
+
+def set_state():
+    with open(state_file, 'w') as file:
+        json.dump(
+            [name for name, performed in state if performed],
+            file,
+            indent=2
+        )
