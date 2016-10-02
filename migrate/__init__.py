4ced73661d21e42a34204178cb31ad55d6f097e5
Andriy Maletsky <andriy.maletsky@gmail.com>
2016-10-02 15:18:33 +0300

    ready to start rewriting everything like typical dev

--- migrate/__init__.py
+++ migrate/__init__.py
@@ -2,6 +2,5 @@
 Yet another Python migration tool
 https://github.com/and800/migrations
 """
-from ._runner import create, perform, show
-__version__ = '0.1.0'
-__all__ = ['create', 'perform', 'show']
+__version__ = '0.2.0'
+__all__ = []
---
+++ migrate/legacy/__init__.py
@@ -0,0 +1,7 @@
+"""
+Yet another Python migration tool
+https://github.com/and800/migrations
+"""
+from ._runner import create, perform, show
+__version__ = '0.1.0'
+__all__ = ['create', 'perform', 'show']
